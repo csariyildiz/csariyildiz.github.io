fffcb9b1035b5aa8d2247c9c763eeac9aae2f84d
Çağrı Sarıyıldız <58122280+csariyildiz@users.noreply.github.com>
2024-11-08 06:25:40 +0300

    Update web-project.js

--- web-projects/assets/web-project.js
+++ web-projects/assets/web-project.js
@@ -1,1 +1,14 @@
-$( ".container" ).show();
+// web-project.js
+
+function toggleDarkMode() {
+  $('body').toggleClass('dark-mode');
+  localStorage.setItem('darkMode', $('body').hasClass('dark-mode') ? 'enabled' : 'disabled');
+}
+
+function initializeDarkMode() {
+  if (localStorage.getItem('darkMode') === 'enabled') {
+    $('body').addClass('dark-mode');
+  }
+}
+
+export { toggleDarkMode, initializeDarkMode };
