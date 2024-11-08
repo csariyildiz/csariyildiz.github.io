184b11380d2c9fc46a0539241af3baaec62c05e3
Çağrı Sarıyıldız <58122280+csariyildiz@users.noreply.github.com>
2024-11-08 06:53:11 +0300

    Update script.js

--- web-projects/script.js
+++ web-projects/script.js
@@ -1,8 +1,7 @@
 // script.js
 
 $(document).ready(function() {
- 
-  // Bind dark mode toggle to button
+  
   $('#toggleTheme').click(function() {
     acsWebProject.toggleTheme();
   });
@@ -10,16 +9,4 @@ $(document).ready(function() {
 
   $('.container').fadeIn();
 
-      $('#convertButton').click(function() {
-        const htmlContent = $('#htmlInput').val();
-        
-        // Create a new instance of Showdown's converter
-        const converter = new showdown.Converter({ tables: true, simplifiedAutoLink: true });
-        
-        // Convert HTML to Markdown
-        const markdown = converter.makeMarkdown(htmlContent);
-        
-        // Display the result
-        $('#markdownOutput').text(markdown);
-      });
     });
