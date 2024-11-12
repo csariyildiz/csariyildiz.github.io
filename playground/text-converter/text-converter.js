f28f61f7ef06e49595c1775b0485335d74f62ef7
Çağrı Sarıyıldız <58122280+csariyildiz@users.noreply.github.com>
2024-11-12 05:43:36 +0300

    Create text-converter.js

---
+++ playground/text-converter/text-converter.js
@@ -0,0 +1,25 @@
+// script.js
+
+$(document).ready(function() {
+ 
+  // Bind dark mode toggle to button
+  $('#toggleTheme').click(function() {
+    acsWebProject.toggleTheme();
+  });
+  
+
+  $('.container').fadeIn();
+
+      $('#convertButton').click(function() {
+        const htmlContent = $('#htmlInput').val();
+        
+        // Create a new instance of Showdown's converter
+        const converter = new showdown.Converter({ tables: true, simplifiedAutoLink: true });
+        
+        // Convert HTML to Markdown
+        const markdown = converter.makeMarkdown(htmlContent);
+        
+        // Display the result
+        $('#markdownOutput').text(markdown);
+      });
+    });
